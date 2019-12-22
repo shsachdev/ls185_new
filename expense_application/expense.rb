23a633c7ddc0385f681135ed27e7761581ba8b46
Shikhar Sachdev <shsachdev@gmail.com>
2019-12-22 10:38:25 -0800

    added CLI class

--- expense_application/expense.rb
+++ expense_application/expense.rb
@@ -2,17 +2,20 @@
 
 require 'pg'
 
+
 class ExpenseData
+  def initialize
+    @connection = PG.connect(dbname: "expenses")
+  end
+
   def add_expenses(amount, memo)
     date = Date.today
     sql = "INSERT INTO expenses (amount, memo, created_on) VALUES ($1, $2, $3)"
-    CONNECTION.exec_params(sql, [amount, memo, date])
+    @connection.exec_params(sql, [amount, memo, date])
   end
 
   def list_expenses
-    connection = PG.connect(dbname: "expenses")
-
-    result = connection.exec("SELECT * FROM expenses ORDER BY created_on ASC")
+    result = @connection.exec("SELECT * FROM expenses ORDER BY created_on ASC")
     result.each do |tuple|
       columns = [tuple["id"].rjust(3), tuple["created_on"].rjust(10),
       tuple["amount"].rjust(12),
@@ -23,33 +26,39 @@ class ExpenseData
   end
 end
 
-CONNECTION = PG.connect(dbname: "expenses")
+class CLI
+  def initialize
+    @application = ExpenseData.new
+  end
 
+  def run(arguments)
+    command = arguments.shift
+    case command
+    when "list"
+      @application.list_expenses
+    when "add"
+      amount = ARGV[1]
+      memo = ARGV[2]
+      abort "You must provide an amount and memo." unless amount && memo
+      @application.add_expenses(amount, memo)
+    else
+      display_help
+    end
+  end
 
-def display_help
-  puts <<~HELP
-    An expense recording system
+  def display_help
+    puts <<~HELP
+      An expense recording system
 
-    Commands:
+      Commands:
 
-    add AMOUNT MEMO [DATE] - record a new expense
-    clear - delete all expenses
-    list - list all expenses
-    delete NUMBER - remove expense with id NUMBER
-    search QUERY - list expenses with a matching memo field
-  HELP
+      add AMOUNT MEMO [DATE] - record a new expense
+      clear - delete all expenses
+      list - list all expenses
+      delete NUMBER - remove expense with id NUMBER
+      search QUERY - list expenses with a matching memo field
+    HELP
+  end
 end
 
-command = ARGV.first
-
-case command
-when "list"
-  list_expenses
-when "add"
-  amount = ARGV[1]
-  memo = ARGV[2]
-  abort "You must provide an amount and memo." unless amount && memo
-  add_expenses(amount, memo)
-when nil
-  display_help
-end
+CLI.new.run(ARGV)
