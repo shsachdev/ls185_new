e55d24f9c977bd2f2425012f12ab40ab1f65f127
Shikhar Sachdev <shsachdev@gmail.com>
2019-12-22 10:29:33 -0800

    moved methods into expensedata class

--- expense_application/expense.rb
+++ expense_application/expense.rb
@@ -2,22 +2,28 @@
 
 require 'pg'
 
-CONNECTION = PG.connect(dbname: "expenses")
+class ExpenseData
+  def add_expenses(amount, memo)
+    date = Date.today
+    sql = "INSERT INTO expenses (amount, memo, created_on) VALUES ($1, $2, $3)"
+    CONNECTION.exec_params(sql, [amount, memo, date])
+  end
 
-# code structure needs to be changed
+  def list_expenses
+    connection = PG.connect(dbname: "expenses")
 
-def list_expenses
-  connection = PG.connect(dbname: "expenses")
+    result = connection.exec("SELECT * FROM expenses ORDER BY created_on ASC")
+    result.each do |tuple|
+      columns = [tuple["id"].rjust(3), tuple["created_on"].rjust(10),
+      tuple["amount"].rjust(12),
+      tuple["memo"]]
 
-  result = connection.exec("SELECT * FROM expenses ORDER BY created_on ASC")
-  result.each do |tuple|
-    columns = [tuple["id"].rjust(3), tuple["created_on"].rjust(10),
-    tuple["amount"].rjust(12),
-    tuple["memo"]]
-
-    puts columns.join(" | ")
+      puts columns.join(" | ")
+    end
   end
 end
+
+CONNECTION = PG.connect(dbname: "expenses")
 
 
 def display_help
@@ -32,14 +38,6 @@ def display_help
     delete NUMBER - remove expense with id NUMBER
     search QUERY - list expenses with a matching memo field
   HELP
-end
-
-# adding a change
-
-def add_expenses(amount, memo)
-  date = Date.today
-  sql = "INSERT INTO expenses (amount, memo, created_on) VALUES ($1, $2, $3)"
-  CONNECTION.exec_params(sql, [amount, memo, date])
 end
 
 command = ARGV.first
